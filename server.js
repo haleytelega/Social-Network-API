28ed6adb4eeb237743824654a9f3515d6fa3a251
Haley Telega <haleytelega@outlook.com>
2022-06-17 15:56:13 -0400

    working on routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.use(require('./routes'));
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
     useNewUrlParser: true,
