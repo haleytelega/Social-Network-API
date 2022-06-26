f231792d2cda82e854bd23385851df81ea459554
Haley Telega <haleytelega@outlook.com>
2022-06-26 14:11:51 -0400

    everything is working

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,8 +12,7 @@ const thoughtController = {
     },
 
     //creating a new Thought
-    createThought({ params, body }, res) {
-        console.log('---------->', body)
+    createThought({ body }, res) {
         Thought.create(body)
             .then(({ _id }) => {
                 return User.findOneAndUpdate(
@@ -42,6 +41,32 @@ const thoughtController = {
         });
     },
     
+    getReaction({ params,body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ message: 'No reaction found with this id!' });
+                return;
+            }
+                res.json(user);
+            })
+            .catch(err => res.json(err));
+        },
+
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: params.reactionsId } },
+            { new: true },
+        )
+        .then(thoughts => res.json(thoughts))
+        .catch(err => res.json(err))
+    },
+
     //update Thought by id
     updateThought({ params, body }, res) {
         Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true }) //must have new: true to return new version of the document
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAllThought, createThought, getThoughtById, updateThought, deleteThought } = require('../../controllers/thought-controller');
+const { getAllThought, createThought, getThoughtById, updateThought, deleteThought, getReaction, removeReaction } = require('../../controllers/thought-controller');
 
 router
     .route('/')
@@ -12,5 +12,11 @@ router
     .get(getThoughtById)
     .put(updateThought)
     .delete(deleteThought);
+
+router
+    .route('/:thoughtId/reactions').post(getReaction);
+
+router
+    .route('/:thoughtId/:reactionsId').delete(removeReaction);
 
 module.exports = router;
